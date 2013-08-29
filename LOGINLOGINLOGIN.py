60f08231ab51b2e0e59e845ec12966171a153ad2
Joseph Utecht <thenmal@gmail.com>
2013-08-29 14:38:19 -0500

    added win7 branch

--- LOGINLOGINLOGIN.py
+++ LOGINLOGINLOGIN.py
@@ -1,24 +1,20 @@
 import os
 import time
 import sys
-import ImageGrab
+from PIL import ImageGrab
 
-import ImageOps
+from PIL import ImageOps
 
 import win32api
 import win32gui
 import win32con
 
-
-
 def windowEnumerationHandler(hwnd, resultList):
-
     '''Pass to win32gui.EnumWindows() to generate list of window handle, window text tuples.'''
-
     resultList.append((hwnd, win32gui.GetWindowText(hwnd)))
 
 def sendKeys(handle):
-    print "Sending NUMPAD0..."
+    print("Sending NUMPAD0...")
     win32api.SendMessage(handle, win32con.WM_KEYDOWN, 0x60, 0)
     win32api.SendMessage(handle, win32con.WM_KEYUP, 0x60, 0)
 
@@ -30,10 +26,10 @@ def get_handle():
     for wnd,title in topWindows:
         if title == "FINAL FANTASY XIV: A Realm Reborn":
             handle = wnd
-            print "Found the window. Everything looks good."
+            print("Found the window. Everything looks good.")
 
     if handle is None:
-        print "WoW window not found. Is it running?"
+        print("WoW window not found. Is it running?")
         sys.exit(1)
 
     return handle
@@ -48,18 +44,17 @@ def isError1017():
     im = screenGrab()
     # test to see if it's an error message
 
-    reference_code = (239, 194, 53)
-    reference_background = (16, 16, 16)
+    reference_code = (238, 190, 48)
+    reference_background = (13, 13, 13)
 
     error_code_pixels = [
-        im.getpixel((728, 646)),
-        im.getpixel((746, 647)),
-        im.getpixel((746, 652)),
+        im.getpixel((728, 634)),
+        im.getpixel((746, 636)),
     ]
 
     box_background_pixels = [
-        im.getpixel((681, 620)),
-        im.getpixel((379, 636)),
+        im.getpixel((687, 625)),
+        im.getpixel((674, 641)),
     ]
 
     for p in error_code_pixels:
@@ -76,43 +71,33 @@ def isCharSelectScreen():
     im = screenGrab()
     # white char box and realm square
     white_stuff = [
-        im.getpixel((587, 92)),
-        im.getpixel((661, 92)),
-        im.getpixel((709, 117)),
-        im.getpixel((580, 149)),
-        im.getpixel((676, 163)),
+        im.getpixel((653, 85)),
+        im.getpixel((867, 107)),
 
         # character name, gaurantees loaded
-        im.getpixel((632, 179)),
-        im.getpixel((664, 177)),
-        im.getpixel((616, 177)),
+        im.getpixel((596, 171))
     ]
 
-    print white_stuff
+    print(white_stuff)
 
     for p in white_stuff:
         if p != (255, 255, 255):
             return False
-
     return True
-
 
 def send0():
     sendKeys(handle)
     time.sleep(0.5)
 
-
 im = screenGrab()
-print isCharSelectScreen()
-print isError1017()
+print(isCharSelectScreen())
+print(isError1017())
 
 #exit()
 
-
-
 handle = get_handle()
 
-print "waiting 2 seconds"
+print("waiting 2 seconds")
 time.sleep(2)
 
 # assume we're at the login screen, start with two 0's
@@ -122,7 +107,7 @@ send0()
 while True:
     # wait for the character screen
     while not isCharSelectScreen():
-        print "Character not loading?"
+        print("Character not loading?")
         time.sleep(1)
 
     send0()
@@ -130,14 +115,8 @@ while True:
 
     # now find the error screen
     while not isError1017():
-        print "Error not loading?"
+        print("Error not loading?")
         time.sleep(1)
 
-
     send0()
     send0()
-
-
-
-
-
